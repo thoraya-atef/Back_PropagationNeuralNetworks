cad8855ea6879101325e6c12cdc9f2cae4743880
thoraya-atef <thorayaatef22@gmail.com>
2022-01-04 23:22:46 +0200

    final

--- src/NN.java
+++ src/NN.java
@@ -35,6 +35,7 @@ public class NN {
             Y.add(i,y);
 
         }
+        Y = GaussianNormalization(Y, M);
         vector = GaussianNormalization(X, M);
 
         return vector;
@@ -42,36 +43,36 @@ public class NN {
     }
 
     // ***************Normalization****************
-    public static double CalculateMean(Vector<double[]> X, int idx) {
+    public static double CalculateMean(Vector<double[]> v, int idx) {
         double sum = 0.0;
         for (int i = 0; i < K;i++) {
-            sum += X.get(i)[idx];
+            sum +=v.get(i)[idx];
         }
         double mean=sum/K;
         return mean;
     }
 
-    public static double CalculateStandardDiv(Vector<double[]> X, int idx, double Mean) {
+    public static double CalculateStandardDiv(Vector<double[]> V, int idx, double Mean) {
         double standardDiv = 0.0;
         for (int j = 0; j < K;j++) {
-            standardDiv+= Math.pow(X.get(j)[idx] - Mean, 2);
+            standardDiv+= Math.pow(V.get(j)[idx] - Mean, 2);
         }
         standardDiv = Math.sqrt(standardDiv / K);
         return standardDiv;
     }
 
-    public static Vector<double[]> GaussianNormalization(Vector<double[]> X, int M) {
+    public static Vector<double[]> GaussianNormalization(Vector<double[]> V, int M) {
         Vector<double[]> vector = new Vector<>();
         double normalized[] = new double[M];
-        double[]  Mean = new double[M];
-        double[] std_dev = new double[M];
-        for (int i = 0; i < M; i++){
-            Mean[i] = CalculateMean(X, i);
-            std_dev[i] = CalculateStandardDiv(X, i,Mean[i]);
+        double[]  Mean = new double[V.get(0).length];
+        double[] std_dev = new double[V.get(0).length];
+        for (int i = 0; i < V.get(0).length; i++){
+            Mean[i] = CalculateMean(V, i);
+            std_dev[i] = CalculateStandardDiv(V, i,Mean[i]);
         }
         for (int j = 0; j < K; j++) {
-            for (int i = 0; i < M; i++) {
-                normalized[i] = (X.get(j)[i] -  Mean[i]) / std_dev[i];
+            for (int i = 0; i < V.get(0).length; i++) {
+                normalized[i] = (V.get(j)[i] -  Mean[i]) / std_dev[i];
             }
             vector.add(j, normalized);
         }
@@ -141,7 +142,7 @@ public class NN {
     public static double[] FeedForward_output(double weights[][], double Hidden[]){
         double result[] = new double[weights.length];
         double[] resultofsum = SumofXdotW(weights, Hidden);
-        for (int i = 0; i < resultofsum.length; i++) {
+        for (int i = 0; i < N; i++) {
             result[i] = SigmoidFunction(resultofsum[i]);
         }
         return result;
@@ -150,50 +151,48 @@ public class NN {
         double Error[] = new double[N];
         double Y_error[] = new double[N];
         for (int i = 0; i < N; i++) {
-            Error[i] = predicted[i] - Y_target[i];
-        }
-        for (int i = 0; i < N; i++) {
+            Error[i] = Y_target[i]-predicted[i];
             Y_error[i] = predicted[i] * (1 - predicted[i]) * Error[i];
         }
         return Y_error;
     }
-    public static double[] ErrorofHiddenlayer(double Y_error[],double H[], double Weights_hiddenLayer[][]){
+    public static double[] ErrorofHiddenlayer(double Y_error[],double H[], double Weights_outputLayer[][]){
         double Hidden_error[] = new double[L];
         for (int i = 0; i < L; i++) {
             double Sum = 0;
             for (int j = 0; j < N; j++) {
-                Sum += (Y_error[j] * Weights_hiddenLayer[i][j]);
+                Sum += (Y_error[j] * Weights_outputLayer[j][i]);
             }
             Hidden_error[i] = H[i] * (1 - H[i]) * Sum;
         }
         return Hidden_error;
     }
     public static double[][] Updated_weights_of_outputlayer(double[][] old_Weights, double learning_rate,
-                                                     double[] L_out, double[] Y_error){
+                                                     double[] Hidden_out, double[] Y_error){
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < L; j++) {
-                old_Weights[i][j] = old_Weights[i][j] - learning_rate * L_out[j] * Y_error[i];
+                old_Weights[i][j] = old_Weights[i][j] + learning_rate * Hidden_out[j] * Y_error[i];
             }
         }
         return old_Weights;
     }
     public static double[][] Updated_weights_of_Hiddenlayer(double[][] old_Weights, double learning_rate,
-                                                     double[] M_out, double[] Hidden_error){
+                                                     double[] x, double[] Hidden_error){
         for (int i = 0; i < L; i++) {
             for (int j = 0; j < M; j++) {
-                old_Weights[i][j] = old_Weights[i][j] - learning_rate * M_out[j] * Hidden_error[i];
+                old_Weights[i][j] = old_Weights[i][j] + learning_rate * x[j] * Hidden_error[i];
 
             }
         }
         return old_Weights;
     }
-    public static double Mean_Sq_error(double Y_targetl[], double Y_Predicted[]){
+    public static double Sq_error(double Y_targetl[], double Y_Predicted[]){
         double error = 0;
         for (int i = 0; i < N; i++) {
             error += Math.pow(Y_targetl[i] - Y_Predicted[i], 2);
         }
-        error /= N;
+        error =error*.5;
         return error;
     }
     ///*******************First Program*****************
@@ -211,28 +210,46 @@ public class NN {
         Weights_hidden_layer=new Vector<>();
         Weights_output_layer=new Vector<>();
         double learning_rate = 0.1;
-        double MSerors[] = new double[K];
+        double erors[] = new double[K];
+        double MSerors=100000;
+        double Min=20000;
         WeightsHidden = Weights(WeightsHidden,L,M);
-
         WeightsOutput = Weights(WeightsOutput,N,L);
         for (int i=0;i<num_of_iterations;i++){
             for(int j=0;j<K;j++){
                 Hidden=FeedForward_hidden(WeightsHidden,v.get(j));
                 Output=FeedForward_output(WeightsOutput,Hidden);
+
                 Errors_Ys=ErrorofOutputlayer(Y.get(j),Output);
-                Errors_Hs=ErrorofHiddenlayer(Errors_Ys,Hidden,WeightsHidden);
+                Errors_Hs=ErrorofHiddenlayer(Errors_Ys,Hidden,WeightsOutput);
                 WeightsOutput=Updated_weights_of_outputlayer(WeightsOutput,learning_rate,Hidden,Errors_Ys);
+
                 WeightsHidden=Updated_weights_of_Hiddenlayer(WeightsHidden,learning_rate,v.get(j),Errors_Hs);
                 if(i==num_of_iterations-1) {
                     Weights_hidden_layer.add(j, WeightsHidden);
                     Weights_output_layer.add(j, WeightsOutput);
-                    MSerors[j] = Mean_Sq_error(Y.get(j), Output);
-                    System.out.println("Mean Square Error of  = " +(j+1)+ MSerors[j] + "\n");
                 }
+                erors[j] = Sq_error(Y.get(j), Output);
+
 
             }
+            double sumError=0.0;
+            for(int j=0;j<K;j++){
+                sumError+=erors[j];
+            }
+            sumError=sumError/=K;
+
+            if(sumError<Min)
+            {
+                MSerors=sumError;
+            }
+           System.out.println("Mean Square Error of iteration "+(i+1)+" = "+sumError);
+
 
         }
+
+        System.out.println(" best Mean Square Error  = "+MSerors);
+
     }
     public static void SecondProgram() throws IOException {
         Vector<double[]>v= new Vector<>();
@@ -247,9 +264,15 @@ public class NN {
         for(int j=0;j<K;j++) {
             Hidden = FeedForward_hidden(Weights_hidden_layer.get(j), v.get(j));
             Output = FeedForward_output(Weights_output_layer.get(j), Hidden);
-            MSerors[j] = Mean_Sq_error(Y.get(j), Output);
-            System.out.println("Mean Square Error of  = " +(j+1)+ MSerors[j] + "\n");
+            MSerors[j] = Sq_error(Y.get(j), Output);
         }
+        double sumerror=0.0;
+        for(int j=0;j<K;j++){
+            sumerror+=MSerors[j];
+        }
+        sumerror/=K;
+        System.out.println("Mean Square Error = "+sumerror);
+
 
     }
 
